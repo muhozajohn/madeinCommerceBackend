61058080b52cf9ccf037d9a666ef7cfd266fc5c1
muhozajohn <muhozajohn250@gmail.com>
2023-03-23 17:23:27 +0200

    ft configure database

--- src/dbase/models/index.js
+++ src/dbase/models/index.js
@@ -1,37 +1,51 @@
-'use strict';
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const { TEST_DB } = require("../../test/dbconfig");
+
+const env = TEST_DB;
+const config = require(`${__dirname}/../config/config`)[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (config.url) {
+  try {
+    sequelize = new Sequelize(config.url, { logging: false });
+    sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  try {
+    sequelize = new Sequelize(
+      config.database,
+      config.username,
+      config.password,
+      config
+    );
+    console.log("");
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
+fs.readdirSync(__dirname)
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
     );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
